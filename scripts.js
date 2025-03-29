dbf5507a091ae3edecd458783477ebe45d8f1557
Surghe <Milesiii.Irving@gmail.com>
2025-03-28 23:08:38 -0600

    second commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Part 2 Dom Manipulation
+
+// 3.-----------------------------------------------------------------
+let subMenuEl = document.getElementById('nav');
+subMenuEl.style.height = 100%;
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add('flex-around');
+
+// 4.---------------------------------------------------------------------

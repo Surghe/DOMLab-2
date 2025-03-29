41519a80375fdd5670e58e486407c891647f7f19
Surghe <Milesiii.Irving@gmail.com>
2025-03-28 23:25:01 -0600

    final commit

--- scripts.js
+++ scripts.js
@@ -7,3 +7,22 @@ subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add('flex-around');
 
 // 4.---------------------------------------------------------------------
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
+
+  let topMenuLinks = document.querySelector('a');
